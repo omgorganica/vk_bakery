c858980cac3b97120717d8ee22b435708e16d718
omgorganica@yandex.ru <Rmztv9if>
2021-05-26 14:39:15 +0500

    main

--- server_manager.py
+++ server_manager.py
@@ -6,6 +6,7 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 COMMUNITY = os.getenv("COMMUNITY_ID")
 
-server1 = Server(TOKEN, COMMUNITY, "Metall-Bakery")  # Создание нового сервера
-server1.test()  # Проверка работы бота
-server1.start()  # Запуск бота
+if __name__ == "__main__":
+    server1 = Server(TOKEN, COMMUNITY, "Metall-Bakery")  # Создание нового сервера
+    server1.test()  # Проверка работы бота
+    server1.start()  # Запуск бота
